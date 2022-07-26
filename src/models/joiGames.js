3903db11810a4dd37b0b993398f5bd7f7ac4070e
Lucas <bazezimo42@gmail.com>
2022-07-26 00:56:06 -0300

    fix: joi schemas missing 'min(1)'

--- src/models/joiGames.js
+++ src/models/joiGames.js
@@ -4,7 +4,7 @@ const gamesSchema = joi.object({
     name: joi.string().min(1).required(),
     image: joi.string().min(1).required(),
     stockTotal: joi.number().min(1).required(),
-    categoryId: joi.number().required(),
+    categoryId: joi.number().min(1).required(),
     pricePerDay: joi.number().min(1).required()
 });
 
